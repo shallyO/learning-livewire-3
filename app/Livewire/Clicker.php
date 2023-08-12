f8d25f6a4e342a5688ae5bddb5331f05441a00ef
shalom ogbeche <shalomogbeche@gmail.com>
2023-08-12 10:34:29 +0100

    learnt flassmessage and and formfield reset

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -3,19 +3,32 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Clicker extends Component
 {
-    public $name;
-    public $email;
-    public $password;
+    #[Rule('required|min:2|max:50')]
+    public $name = '';
+
+    #[Rule('required|min:2|max:50')]
+    public $email = '';
+
+    #[Rule('required|min:2')]
+    public $password = '';
     public function createNewUser(){
+
+        $this->validate();
+
         User::create([
             'name' => $this->name,
             'email' => $this->email,
             'password' => $this->password
         ]);
+
+        $this->reset(['name', 'email', 'password']);
+
+        request()->session()->flash('success', 'User Created Successfully');
     }
     public function render()
     {
--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,15 +1,31 @@
 <div>
+    @if(session('success'))
+        <span class="w-100 px-3 py-3 bg-green-600 rounded text-white">{{ session('success') }}</span>
+    @endif
     <br>
     <form action="" class="ml-2" wire:submit="createNewUser">
-        <input wire:model="name" type="text" placeholder="name">
-        <input wire:model="email" type="text" placeholder="email">
-        <input wire:model="password" type="text" placeholder="password">
+        <input wire:model="name" type="text" placeholder="name" class="block rounded border border-gray-100 px-3 py-1 mb-1">
+        @error('name')
+            <span class="text-red-500 text-xs"> {{ $message }}</span>
+        @enderror
+        <input wire:model="email" type="text" placeholder="email" class="block rounded border border-gray-100 px-3 py-1 mb-1">
+        @error('email')
+        <span class="text-red-500 text-xs"> {{ $message }}</span>
+        @enderror
+        <input wire:model="password" type="text" placeholder="password" class="block rounded border border-gray-100 px-3 py-1 mb-1">
+        @error('password')
+        <span class="text-red-500 text-xs"> {{ $message }}</span>
+        @enderror
+        <button class="block rounded px-3 py-1 border bg-gray-400 text-white">Create </button>
 
-        <button>Create </button>
+    </form>
 
-        <br><br>
+    <br>
+    <hr>
+    <div class="ml-2">
         @foreach($users as $user)
             <h1> {{ $user->name }} </h1>
         @endforeach
-    </form>
+    </div>
+
 </div>
