6d18894a72b04c326d7cc30d992a6f4ea922bda5
shalom ogbeche <shalomogbeche@gmail.com>
2023-08-12 09:42:28 +0100

    learning how to pass data from Livewire Controller component to view

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -2,15 +2,28 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Clicker extends Component
 {
-    public function handleClick(){
-        dump('clicked');
+    public $username = 'tester';
+    public function createNewUser(){
+        User::create([
+            'name' => 'test user',
+            'email' => 'test@test2.com',
+            'password' => '560948393434'
+        ]);
     }
     public function render()
     {
-        return view('livewire.clicker');
+        $title = "Shaloms first livewire";
+
+        $users = User::all();
+        return view('livewire.clicker',
+            [
+                'users' => $users,
+                'title' => $title
+            ]);
     }
 }
--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,3 +1,10 @@
 <div>
-    <button wire:click="handleClick"> Click Me</button>
+    <h1> {{ $username }}</h1>
+
+    <h1> {{ $title }}</h1>
+
+    {{ count($users) }}
+    <button wire:click="createNewUser" class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+        Create New User
+    </button>
 </div>
